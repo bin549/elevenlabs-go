f1de578e22563e02f03dd35f0bf184d2c99d513b
--# <--#@gmail.com>
2024-04-11 13:39:31 +0800

    test: 1

--- models.go
+++ models.go
@@ -39,7 +39,7 @@ type TextToSpeechRequest struct {
 }
 
 type SpeechToSpeechRequest struct {
-	audio         []byte         `json:"audio"`
+	Audio         []byte         `json:"audio"`
 	ModelID       string         `json:"model_id,omitempty"`
 	VoiceSettings *VoiceSettings `json:"voice_settings,omitempty"`
 }
